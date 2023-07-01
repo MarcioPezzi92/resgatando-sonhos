f2ba572ee99ca7ce59ccf450f19cd6dc1656f218
Marcio Pezzi <102781934+MarcioPezzi92@users.noreply.github.com>
2023-07-01 16:19:50 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { GalleriaModule } from 'primeng/galleria';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePage } from './pages/home/home.page';
+import { HeaderComponent } from './components/header/header.component';
+import { GalleryComponent } from './pages/home/components/gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePage,
+    HeaderComponent,
+    GalleryComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GalleriaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
